c6db7fcefd52c4b83c5c1d6cdbf2291f571e79d9
Pawlo77 <pawpoz16@gmail.com>
2023-04-28 16:44:17 +0200

    finished_eda

--- src/utils/eda.py
+++ src/utils/eda.py
@@ -1,6 +1,8 @@
 import os
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 PLOTS_DIR = "plots"
 
@@ -17,3 +19,32 @@ def save_fig(
     """
     os.makedirs(dir, exist_ok=True)
     plt.savefig(os.path.join(dir, name + ".png"), **kwargs)
+
+
+def finish(
+    ax: plt.Axes,
+    title: str,
+    title_size: int = 20,
+    xaxis_size: int = 13,
+    yaxis_size: int = 13,
+    xticks_size: int = 9,
+    yticks_size: int = 9,
+) -> None:
+    """
+    Pretty finishes matplotlib plot, saves it and opens the plot.
+    :param ax: The plot object
+    :param title: The title to be set (also filename)
+    :param title_size: Title font size
+    :param xaxis_size: X axis font size
+    :param yaxis_size: Y axis font size
+    :param xticks_size: X ticks labels font size
+    :param yticks_size: Y ticks labels font size
+    """
+    sns.despine()
+    ax.xaxis.label.set_size(xaxis_size)
+    ax.yaxis.label.set_size(yaxis_size)
+    ax.set_yticklabels(ax.get_yticks().astype(np.int64), size=xticks_size)
+    ax.set_xticklabels(ax.get_xticklabels(), size=yticks_size)
+    plt.title(title, size=title_size)
+    save_fig(title)
+    plt.show()
