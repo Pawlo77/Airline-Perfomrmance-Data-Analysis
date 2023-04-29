106674f0aa7c61e87312f57e3219bc52090371e2
Igor <kolodziej.igor@outlook.com>
2023-04-29 18:00:55 +0200

    created script downloading additional airport data from other source

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,8 @@
 from .load_data import load_flights, load_airports, load_carriers, load_plane_data
+from .load_airports_additional import load_airports_details
 
 load_flights = load_flights
 load_airports = load_airports
 load_carriers = load_carriers
 load_plane_data = load_plane_data
+load_airports_details = load_airports_details
