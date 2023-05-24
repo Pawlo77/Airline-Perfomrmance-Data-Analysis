85c42ddf4653d274cb5ebead00d2a57441d25e16
Igor Kolodziej <kolodziej.igor@outlook.com>
2023-05-25 00:00:25 +0200

    improvements

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,10 +3,11 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 from .data_preparation import (
+    load_airports_details,
     load_flights,
     load_airports,
     load_carriers,
-    load_plane_data,
+    load_plane_data
 )
 
 from .charts import generate_charts
---
+++ src/utils/data_preparation/load_airports_additional.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+from .constants import DATASETS_FOLDER
+
+URL = 'https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat'
+
+def load_airports_details():
+    try:
+        airports_details = pd.read_pickle(os.path.join(DATASETS_FOLDER, "airports_details.pkl"))
+    except:
+        airports_details = pd.read_csv(URL, header=None, names=['airportID', 'name', 'city', 'country', 'iata', 'icao', 'lat', 'lon', 'altitude', 'timezone', 'dst', 'tz', 'type', 'source'])
+        airports_details.to_pickle(os.path.join(DATASETS_FOLDER, "airports_details.pkl"))
+    return airports_details
